3bcd943e4c8bc77bd206a2f6a8275a293432f188
peter.pinter <pinterpeti@gmail.com>
2020-10-02 15:44:57 +0200

    Fixed prelling of the remote stalk buttons on Junsun Android head units with Raise CAN box

--- src/Can/Handlers/CanRadioRemoteMessageHandler.h
+++ src/Can/Handlers/CanRadioRemoteMessageHandler.h
@@ -10,18 +10,30 @@
 
 class CanRadioRemoteMessageHandler : public CanMessageHandlerBase
 {
-    static const uint16_t CAN_RADIO_REMOTE_MESSAGE_INTERVAL = 50;
+    static const uint16_t CAN_RADIO_REMOTE_MESSAGE_INTERVAL = 500;
 
     CanRadioRemoteButtonPacketSender* RadioRemoteSender;
+
     uint8_t ButtonByte;
     uint8_t ScrollByte;
 
+    uint8_t PrevScrollByte;
+    bool SendEmpty = false;
+
     virtual void InternalProcess()
     {
-        RadioRemoteSender->SendAsByte(
-            ButtonByte,
-            ScrollByte
-        );
+        if (IS_ANDROID_HEAD_UNIT_INSTALLED == true)
+        {
+            if (SendEmpty)
+            {
+                RadioRemoteSender->SendAsByte(0x00, 0x00);
+                SendEmpty = false;
+            }
+        }
+        else
+        {
+            RadioRemoteSender->SendAsByte(ButtonByte, ScrollByte);
+        }
     }
 
     public:
@@ -35,12 +47,32 @@ class CanRadioRemoteMessageHandler : public CanMessageHandlerBase
         uint8_t scrollByte
     )
     {
-        ButtonByte = buttonByte;
-        ScrollByte = scrollByte;
+        if (IS_ANDROID_HEAD_UNIT_INSTALLED == true)
+        {
+            if (PrevScrollByte == scrollByte)
+            {
+                scrollByte = 0x00;
+            }
+            else
+            {
+                PrevScrollByte = scrollByte;
+            }
 
-        RadioRemoteSender->SendAsByte(
-            ButtonByte,
-            ScrollByte);
+            const bool sendData = buttonByte > 0 || scrollByte != 0;
+
+            if (sendData)
+            {
+                RadioRemoteSender->SendAsByte(buttonByte, scrollByte);
+                SendEmpty = true;
+            }
+        }
+        else
+        {
+            ButtonByte = buttonByte;
+            ScrollByte = scrollByte;
+
+            RadioRemoteSender->SendAsByte(ButtonByte, ScrollByte);
+        }
     }
 };
 
