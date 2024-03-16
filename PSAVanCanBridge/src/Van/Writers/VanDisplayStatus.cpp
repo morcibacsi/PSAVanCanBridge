186d2cce449911c6afb0a6c2083783812ae3cedb
morcibacsi <pinterpeti@gmail.com>
2024-03-16 20:54:57 +0100

    Only send 5E4 when absolutely necessary

--- PSAVanCanBridge/src/Van/Writers/VanDisplayStatus.cpp
+++ PSAVanCanBridge/src/Van/Writers/VanDisplayStatus.cpp
@@ -1,45 +1,81 @@
 #include "VanDisplayStatus.h"
 
-void VanDisplayStatus::SendStatus(uint8_t resetTotals, uint8_t resetCumulative)
+void VanDisplayStatus::SendStatus(uint8_t resetTotals, uint8_t resetCumulative, uint8_t keepVanComfortAlive)
 {
     for (int i = 0; i < SEND_RESET_COUNT; ++i)
     {
-        displayStatusSender->SendStatus(SEND_STATUS_CHANNEL, resetTotals, resetCumulative);
+        displayStatusSender->SendStatus(SEND_STATUS_CHANNEL, resetTotals, resetCumulative, keepVanComfortAlive);
         delay(5);
     }
+}
+
+void VanDisplayStatus::TripReset()
+{
+    uint8_t resetTotals = 0;
+    uint8_t resetCumulative = 0;
+    if (_whichTripToReset == 2) //ODOMETER_TRIP_INFO_TRIP1 from CAN message with id 0x167
+    {
+        resetTotals = 1;
+    }
+    if (_whichTripToReset == 4) //ODOMETER_TRIP_INFO_TRIP2 from CAN message with id 0x167
+    {
+        resetCumulative = 1;
+    }
+    if (_whichTripToReset == 7) //ODOMETER_TRIP_INFO_NOT_MANAGED_BY_EMF from CAN message with id 0x167
+    {
+        resetTotals = 1;
+        resetCumulative = 1;
+    }
+    SendStatus(resetTotals, resetCumulative, 1);
 }
 
 void VanDisplayStatus::InternalProcess()
 {
     if (_ignition)
     {
-        if (_resetTrip == 1 && _resetSent == 0)
+        switch (_resetState)
         {
-            _resetTrip =  0;
-            uint8_t resetTotals = 0;
-            uint8_t resetCumulative = 0;
-            if (_whichTripToReset == 2) //ODOMETER_TRIP_INFO_TRIP1 from CAN message with id 0x167
-            {
-                resetTotals = 1;
-            }
-            if (_whichTripToReset == 4) //ODOMETER_TRIP_INFO_TRIP2 from CAN message with id 0x167
-            {
-                resetCumulative = 1;
-            }
-            if (_whichTripToReset == 7) //ODOMETER_TRIP_INFO_NOT_MANAGED_BY_EMF from CAN message with id 0x167
-            {
-                resetTotals = 1;
-                resetCumulative = 1;
-            }
-            SendStatus(resetTotals, resetCumulative);
-            _resetTrip = 0;
-            _resetSent = 1;
-        }
-        else
-        {
-            displayStatusSender->SendStatus(SEND_STATUS_CHANNEL, 0, 0);
+            case 1:
+                SendStatus(0, 0, 1);
+                _resetState = 2;
+                break;
+            case 2:
+                TripReset();
+                _resetState = 3;
+                break;
+            case 3:
+                SendStatus(0, 0, 1);
+                _resetState = 0;
+                break;
+
+            default:
+                _resetState = 0;
+                break;
         }
     }
+    else
+    {
+        if (_prevIgnition)
+        {
+            _vanComfortState = 1;
+        }
+        switch (_vanComfortState)
+        {
+            case 1:
+                SendStatus(0, 0, 1);
+                _vanComfortState = 2;
+                break;
+            case 2:
+                SendStatus(0, 0, 0);
+                _vanComfortState = 0;
+                break;
+
+            default:
+                _vanComfortState = 0;
+                break;
+        }
+    }
+    _prevIgnition = _ignition;
 }
 
 void VanDisplayStatus::SetData(uint8_t ignition, uint8_t tripButton, uint8_t whichTripToReset, unsigned long currentTime)
@@ -55,14 +91,13 @@ void VanDisplayStatus::SetData(uint8_t ignition, uint8_t tripButton, uint8_t whi
         }
         if (tripButton == 0 && _tripButtonState == 1)
         {
-            _resetTrip = 0;
-            _resetSent = 1;
+            _resetState = 0;
             _tripButtonState = 0;
             _tripButtonPressedTime = 0;//to avoid resetting trip when the button is pressed repeatedly
         }
         if (tripButton == 1 && _tripButtonState == 1 && _tripButtonPressedTime != 0 && currentTime - _tripButtonPressedTime > 3000)
         {
-            _resetTrip = 1;
+            _resetState = 1;
         }
     }
 }
--- PSAVanCanBridge/src/Van/Writers/VanDisplayStatus.h
+++ PSAVanCanBridge/src/Van/Writers/VanDisplayStatus.h
@@ -11,22 +11,23 @@
 
 class VanDisplayStatus : public VanMessageWriterBase
 {
-    const static uint16_t SEND_STATUS_INTERVAL = 420;
+    const static uint16_t SEND_STATUS_INTERVAL = 500;
     const static uint8_t SEND_RESET_COUNT = 1;
 
     // We use the same channel for the status as the trip computer query because otherwise it clashes somehow
     const static uint8_t  SEND_STATUS_CHANNEL = 0;
 
     uint8_t _tripButtonState = 0;
-    uint8_t _resetTrip = 0;
     uint8_t _whichTripToReset = 0;
-    uint8_t _resetSent = 0;
     unsigned long _tripButtonPressedTime = 0;
     uint8_t _ignition = 0;
+    uint8_t _prevIgnition = 0;
+    uint8_t _resetState = 0;
+    uint8_t _vanComfortState = 0;
 
     VanDisplayStatusPacketSender* displayStatusSender;
 
-    void SendStatus(uint8_t resetTotals, uint8_t resetCumulative);
+    void SendStatus(uint8_t resetTotals, uint8_t resetCumulative, uint8_t keepVanComfortAlive);
 
     virtual void InternalProcess() override;
 
@@ -36,6 +37,7 @@ class VanDisplayStatus : public VanMessageWriterBase
     };
 
     void SetData(uint8_t ignition, uint8_t tripButton, uint8_t whichTripToReset, unsigned long currentTime);
+    void TripReset();
 
     void Stop();
 };
