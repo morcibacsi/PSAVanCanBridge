d957d58ec1169de99e37fbb03f5e8f9371b2ccea
morcibacsi <pinterpeti@gmail.com>
2025-09-10 23:04:16 +0200

    Added missing code for 0x128 and 0x168

--- PSAVanCanBridge/src/Can/Handlers/MessageHandler_128.cpp
+++ PSAVanCanBridge/src/Can/Handlers/MessageHandler_128.cpp
@@ -19,10 +19,98 @@ void MessageHandler_128::SetData()
 
 void MessageHandler_128::SetData2004()
 {
+    Can2004CombineLightsByte1 field0;
+    field0.data.fuel_level_low             = _dataBroker->FuelLow;
+    field0.data.driver_seatbelt_warning    = _dataBroker->SeatbeltWarning;
+    field0.data.passenger_seatbelt_warning = _dataBroker->PassengerAirbag;
+    field0.data.handbrake_signal           = _dataBroker->Handbrake;
+    field0.data.air_bag_fault              = _dataBroker->Airbag;
+    field0.data.diesel_pre_heating         = _dataBroker->PreHeatingStatus;
 
+    Can2004CombineLightsByte2 field1;
+    field1.data.abs_active = _dataBroker->AbsFault;
+    field1.data.stop_light = _dataBroker->StopOnCMB;
+    //field1.data.service_indicator_exclamation = 0;
+
+    Can2004CombineLightsByte3 field2;
+    field2.data.esp_inactivated = _dataBroker->EspDeactivated;
+    field2.data.esp_in_progress = _dataBroker->EspInRegulation;
+
+    Can2004CombineLightsByte5 field4;
+    field4.data.right_turn_indicator    = _dataBroker->RightTurnIndicator;
+    field4.data.left_turn_indicator     = _dataBroker->LeftTurnIndicator;
+    field4.data.front_foglight          = _dataBroker->FrontFog;
+    field4.data.rear_foglight           = _dataBroker->RearFog;
+    field4.data.low_beam_on             = _dataBroker->LowBeam;
+    field4.data.high_beam_on            = _dataBroker->HighBeam;
+    field4.data.parking_light_indicator = _dataBroker->SideLights;
+
+    Can2004CombineLightsByte6 field5;
+    field5.data.cmb_active = _dataBroker->OdometerEnabled;
+
+    Can2004CombineLightsByte7 field6;
+    field6.data.gear_position = _dataBroker->GearPosition;
+
+    Can2004CombineLightsByte8 field7;
+    field7.data.auto_gearbox_mode      = _dataBroker->GearBoxMode;
+    field7.data.auto_gearbox_selection = _dataBroker->GearBoxSelection;
+
+    _data[0] = field0.asByte;
+    _data[1] = field1.asByte;
+    _data[2] = field2.asByte;
+    _data[4] = field4.asByte;
+    _data[5] = field5.asByte;
+    _data[6] = field6.asByte;
+    _data[7] = field7.asByte;
 }
 
 void MessageHandler_128::SetData2010()
 {
+    Can2010CombineLightsByte1 field0;
+    field0.data.right_turn_indicator    = _dataBroker->RightTurnIndicator;
+    field0.data.left_turn_indicator     = _dataBroker->LeftTurnIndicator;
+    field0.data.front_foglight          = _dataBroker->FrontFog;
+    field0.data.rear_foglight           = _dataBroker->RearFog;
+    field0.data.low_beam_on             = _dataBroker->LowBeam;
+    field0.data.high_beam_on            = _dataBroker->HighBeam;
+    field0.data.parking_light_indicator = _dataBroker->SideLights;
 
+    Can2010CombineLightsByte2 field1;
+    field1.data.gear_position            = _dataBroker->GearPosition;
+    field1.data.gear_position_in_driving = _dataBroker->GearPosition;
+
+    Can2010CombineLightsByte3 field2;
+    field2.data.auto_gearbox_mode      = _dataBroker->GearBoxMode;
+    field2.data.auto_gearbox_selection = _dataBroker->GearBoxSelection;
+
+    Can2010CombineLightsByte4 field3;
+    field3.data.handbrake_signal              = _dataBroker->Handbrake;
+    field3.data.passenger_airbag_activated    = _dataBroker->PassengerAirbag;
+    field3.data.child_security_active         = !_dataBroker->PassengerAirbag;
+    field3.data.stop_light                    = _dataBroker->StopOnCMB;
+    //field3.data.service_indicator_exclamation = 0;
+
+    Can2010CombineLightsByte5 field4;
+    field4.data.esp_in_progress    = _dataBroker->EspInRegulation;
+    field4.data.esp_inactivated    = _dataBroker->EspDeactivated;
+    field4.data.diesel_pre_heating = _dataBroker->PreHeatingStatus;
+
+    Can2010CombineLightsByte6 field5;
+    field5.data.driver_seatbelt_warning = _dataBroker->SeatbeltWarning;
+    field5.data.fuel_level_low          = _dataBroker->FuelLow;
+
+    Can2010CombineLightsByte7 field6;
+    field6.data.cmb_active = _dataBroker->OdometerEnabled;
+
+    Can2010CombineLightsByte8 field7;
+    field7.data.engine_indicator = _dataBroker->Mil;
+
+    _data[0] = field0.asByte;
+    _data[1] = field1.asByte;
+    _data[2] = field2.asByte;
+    _data[3] = field3.asByte;
+    _data[4] = field4.asByte;
+    _data[5] = field5.asByte;
+    _data[6] = field6.asByte;
+    _data[7] = field7.asByte;
 }
--- PSAVanCanBridge/src/Can/Handlers/MessageHandler_168.cpp
+++ PSAVanCanBridge/src/Can/Handlers/MessageHandler_168.cpp
@@ -19,10 +19,78 @@ void MessageHandler_168::SetData()
 
 void MessageHandler_168::SetData2004()
 {
+    CanDash3Byte1Struct field1;
+    field1.data.auto_gearbox_alert       = _dataBroker->AutoGearboxAlert;
+    field1.data.break_fluid_alert        = _dataBroker->BreakFluidAlert;
+    field1.data.coolant_level_alert      = _dataBroker->CoolantLevelAlert;
+    field1.data.coolant_temp_max         = _dataBroker->CoolantTempMax;
+    field1.data.oil_level_alert          = _dataBroker->OilLevelAlert;
+    field1.data.oil_pressure_alert       = _dataBroker->OilPressureAlert;
+    field1.data.oil_temp_max             = _dataBroker->OilTempMax;
 
+    CanDash3Byte2Struct field2;
+    field2.data.diesel_additive_alert    = _dataBroker->DieselAdditiveAlert;
+    field2.data.fap_clogged              = _dataBroker->FapClogged;
+    field2.data.flat_tyre_alert          = _dataBroker->FlatTyreAlert;
+    field2.data.tyre_pressure_alert      = _dataBroker->TyrePressureAlert;
+
+    CanDash3Byte4Struct field4;
+    field4.data.abs_fault                = _dataBroker->AbsFault;
+    field4.data.brake_pad_fault          = _dataBroker->BrakePadFault;
+    field4.data.esp_fault                = _dataBroker->EspFault;
+    field4.data.mil                      = _dataBroker->Mil;
+    field4.data.gearbox_fault            = _dataBroker->AutoGearboxFault;
+    field4.data.water_in_diesel          = _dataBroker->WaterInDiesel;
+    field4.data.serious_suspension_fault = _dataBroker->SeriousSuspensionFault;
+
+    CanDash3Byte5Struct field5;
+    field5.data.antipollution_fault      = _dataBroker->AntipollutionFault;
+    field5.data.battery_charge_fault     = _dataBroker->BatteryChargeFault;
+    field5.data.diesel_additive_fault    = _dataBroker->DieselAdditiveFault;
+    field5.data.driver_airbag            = _dataBroker->Airbag;
+
+    _data[0] = field1.asByte;
+    _data[1] = field2.asByte;
+    _data[3] = field4.asByte;
+    _data[4] = field5.asByte;
 }
 
 void MessageHandler_168::SetData2010()
 {
+    CanDash3Byte1_2010_Struct field1;
+    field1.data.break_fluid_alert        = _dataBroker->BreakFluidAlert;
+    field1.data.coolant_level_alert      = _dataBroker->CoolantLevelAlert;
+    field1.data.coolant_temp_max         = _dataBroker->CoolantTempMax;
+    field1.data.oil_level_alert          = _dataBroker->OilLevelAlert;
+    field1.data.oil_pressure_alert       = _dataBroker->OilPressureAlert;
 
+    CanDash3Byte2_2010_Struct field2;
+    field2.data.fap_clogged              = _dataBroker->FapClogged;
+    field2.data.flat_tyre_alert          = _dataBroker->FlatTyreAlert;
+    field2.data.tyre_pressure_alert      = _dataBroker->TyrePressureAlert;
+    field2.data.antipollution_fault      = _dataBroker->AntipollutionFault;
+
+    CanDash3Byte3_2010_Struct field3;
+    field3.data.battery_charge_fault     = _dataBroker->BatteryChargeFault;
+    field3.data.serious_suspension_fault = _dataBroker->SeriousSuspensionFault;;
+
+    CanDash3Byte4_2010_Struct field4;
+    field4.data.abs_fault                = _dataBroker->AbsFault;
+    field4.data.brake_pad_fault          = _dataBroker->BrakePadFault;
+    field4.data.esp_fault                = _dataBroker->EspFault;
+    field4.data.mil                      = _dataBroker->Ignition ? _dataBroker->Mil : 0;
+    field4.data.gearbox_fault            = _dataBroker->AutoGearboxFault;
+
+    CanDash3Byte5_2010_Struct field5;
+    field5.data.water_in_diesel          = _dataBroker->WaterInDiesel;
+
+    CanDash3Byte7_2010_Struct field7;
+    field7.data.gearbox_position         = _dataBroker->GearBoxSelection;
+
+    _data[0] = field1.asByte;
+    _data[1] = field2.asByte;
+    _data[2] = field3.asByte;
+    _data[3] = field4.asByte;
+    _data[4] = field5.asByte;
+    _data[7] = field7.asByte;
 }
